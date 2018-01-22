a190849aabb80300bef6ad4c5a624350cb8f9123
jhenrique09 <jhenrique09.mcz@hotmail.com>
2018-01-22 16:31:50 -0200

    potter: Switch to GCC 7.2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -85,8 +85,8 @@ TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_CONFIG := potter_defconfig
 TARGET_KERNEL_SOURCE := kernel/motorola/msm8953
 
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linux-android-4.9/bin
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-androidkernel-
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/aarch64/aarch64-linaro-7.2/bin
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-gnu-
 
 # Audio
 AUDIO_FEATURE_ENABLED_ALAC_OFFLOAD := true
